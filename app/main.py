862402a9a41fe6ed14a2d81823a6385a86959b43
bradmy609 <bradmy609@gmail.com>
2021-12-14 19:28:53 -0500

    updated main.py to include favicon redirect

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
  
 app = Flask(__name__)
  
@@ -6,3 +6,7 @@ app = Flask(__name__)
 def home_view():
         return """<h1>Hello Valley and Vivi, how are you?</h1>
         <h3>I am gay boy</h3>"""
+
+@app.route("/favicon.ico")
+def reroute():
+    return redirect("https://flask-web1738.herokuapp.com/", code=302)
